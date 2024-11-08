7b886ca8aff659e5d905acb5baa1463084234db0
Umm e Aiman <160779331+aiman0168@users.noreply.github.com>
2024-11-08 12:55:12 -0800

    Update page.tsx

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import Image from 'next/image';
-import graphic from '../public/cc.png';
+import graphic from '@public/cc.png';
 
 const About: React.FC = () => {
   return (
