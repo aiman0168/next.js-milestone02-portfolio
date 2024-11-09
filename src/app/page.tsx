07f60026ae463fb821342c32bc93bc22e5394c7b
Umm e Aiman <160779331+aiman0168@users.noreply.github.com>
2024-11-09 09:05:32 -0800

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,10 +8,10 @@ const App: React.FC = () => {
     <div className="bg-white py-12 px-6 sm:px-4 lg:px-8">
       <div className="max-w-screen-lg mx-auto bg-blue-600 text-white p-6 rounded-lg shadow-xl flex justify-center mb-10">
         <div className="text-center">
-          <h3 className="text-xl font-semibold mb-4">Hello! It's Me</h3>
+          <h3 className="text-xl font-semibold mb-4">Hello! Its Me</h3>
           <h1 className="text-3xl sm:text-4xl font-bold mb-4">Umm e Aiman</h1>
           <h3 className="text-2xl sm:text-3xl mb-4">
-            And I'm a <span className="text-yellow-300">Frontend Developer</span>
+            And I am a <span className="text-yellow-300">Frontend Developer</span>
           </h3>
           <div className="mb-4 flex justify-center">
             <Image
