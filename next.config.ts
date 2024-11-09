eb9e5c30c9152b3d4201729a21da96b4fe55708d
Umm e Aiman <160779331+aiman0168@users.noreply.github.com>
2024-11-09 09:25:48 -0800

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,16 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
-};
-
-{
   "rewrites": [
     {
       "source": "/files/:path*",
       "destination": "/public/files/:path*"
     }
   ]
-}
+};
 
 export default nextConfig;
