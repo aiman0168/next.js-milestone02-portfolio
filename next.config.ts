bc8f2ea29e88155b7f4a3cd4d49e21e4c5b50675
Umm e Aiman <160779331+aiman0168@users.noreply.github.com>
2024-11-09 09:36:30 -0800

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-
+  async rewrites() {
+    return [
+      {
+        source: "/cv.pdf",
+        destination: "/public/cv.pdf",  // Just an example of a rewrite rule
+      },
+    ];
+  },
 };
 
 export default nextConfig;
